ab30467c24555daea3f7914707aeda926a2c498b
benxiong.hu <1301536601@qq.com>
2020-07-06 22:32:45 +0800

    新增类和方法调用

---
+++ PythonApi/venv/PythonHelper.py
@@ -0,0 +1,2 @@
+def add(num1,num2):
+    return int(num1)+int(num2)
--- PythonApi/venv/main.py
+++ PythonApi/venv/main.py
@@ -1,10 +1,14 @@
 import sys;
-import  BaseModel
+#import BaseModel
+import PythonApi.venv.PythonHelper as pythonHelper
+from PythonApi.venv.BaseModel import BaseModel
 
 def printHello():
     print("hello")
 
 if "__name__=__main__":
     printHello()
-    a = BaseModel("张三",23);
+    a=BaseModel("李四",12)
     a.sayHi();
+    print(pythonHelper.add(1,2))
+
